7d1b124f8421737a05f6f2e9b1b61ad732ea2689
JDiazD <u23232498@utp.edu.pe>
2026-04-19 12:16:11 -0500

    todo

---
+++ src/main/java/com/helpdesk/helpdesk_backend/service/ProblemaTicketService.java
@@ -0,0 +1,17 @@
+package com.helpdesk.helpdesk_backend.service;
+
+import com.helpdesk.helpdesk_backend.model.ProblemaTicket;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProblemaTicketService {
+    List<ProblemaTicket> listarTodos();
+    Optional<ProblemaTicket> buscarPorId(Long id);
+    ProblemaTicket guardar(ProblemaTicket problemaTicket);
+    ProblemaTicket actualizar(Long id, ProblemaTicket problemaTicket);
+    void eliminar(Long id);
+
+    List<ProblemaTicket> listarPorCategoriaId(Long categoriaId);
+    List<ProblemaTicket> listarPorActivo(boolean activo);
+}
